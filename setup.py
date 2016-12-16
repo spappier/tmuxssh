117354bd5b53467680e304c796e55d72dedaa92e
Santiago Pappier <spappier@gmail.com>
2016-12-16 01:36:38 -0300

    release

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
+import sys
 from setuptools import setup
+
+
+setup_requires = []
+if sys.argv[-1] in ('sdist', 'bdist_wheel'):
+    setup_requires.append('setuptools-markdown')
 
 
 setup(
@@ -8,21 +14,20 @@ setup(
     url='http://github.com/spappier/tmuxssh',
     version='1.0',
     description='ssh into several hosts at once using tmux',
+    long_description_markdown_filename='README.md',
     license='MIT',
     entry_points=dict(console_scripts=['tmuxssh = tmuxssh:main']),
     py_modules=['tmuxssh'],
     scripts=['tmuxssh.py'],
     install_requires=['docopt'],
+    setup_requires=setup_requires,
+    keywords='tmux ssh',
     classifiers=[
         'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
         'License :: OSI Approved :: MIT License',
-        'Environment :: Console',
-        'Intended Audience :: End Users/Desktop',
-        'Natural Language :: English',
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: MacOS :: MacOS X',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
-        'Topic :: Utilities',
     ]
 )
